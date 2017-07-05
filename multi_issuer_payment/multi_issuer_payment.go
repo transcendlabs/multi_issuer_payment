f8c07152390af16639982e550f8f41b60519797f
shubh <ror.shubham@outlook.com>
2017-07-05 18:50:37 +0530

    added 'func' arguemnet in tx

--- multi_issuer_payment/multi_issuer_payment.go
+++ multi_issuer_payment/multi_issuer_payment.go
@@ -18,7 +18,7 @@ import (
 	//"reflect"
 	"crypto/ecdsa"
 	//"crypto/md5"
-	"crypto/rand"
+	//"crypto/rand"
 	"os"
 	"crypto/x509"
 	//"io"
@@ -37,6 +37,7 @@ import (
 type MultiIssuerPaymentApplication struct {
 	types.BaseApplication
 	userAccounts merkle.Tree
+	processorAccounts merkle.Tree
 }
 
 func NewMultiIssuerPaymentApplication() *MultiIssuerPaymentApplication {
@@ -48,89 +49,151 @@ func NewMultiIssuerPaymentApplication() *MultiIssuerPaymentApplication {
 
 func (app *MultiIssuerPaymentApplication) CheckTx(tx []byte) types.Result {
 	parts := strings.Split(string(tx), ",")
-	if len(parts) != 4 {
-		return types.ErrEncodingError.SetLog(cmn.Fmt("Not valid format, format should be of form 'account,tokens,signature,sequence'"))
-	} else {
-		userAddress := string(parts[0])
-		numTokens := parts[1]
-		signature := parts[2]
-		sequence := parts[3]
-		
-		sequenceUint64,_ := strconv.ParseUint(sequence,10,32)
-		sequenceUint := uint32(sequenceUint64)
-		
-		
-		verified := VerifySignature(userAddress, numTokens, sequence, signature)
-		if !verified {
-			return types.ErrEncodingError.SetLog(cmn.Fmt("Signature not verified"))
-		}
-		
-		
-		userAccount := &SmartCardUser{}
-		_, userAccountBytes := accountDetails(app, userAddress)
-		ReadBinaryBytes(userAccountBytes, userAccount) //decoded user_account_bytes to user_account(of type SmartCardUser)
-		//used ReadBinaryBytes(locally implemented) and not wire.ReadBinaryBytes
-		//TODO check what to do if the account doesn't already exists
-	
+	if parts[0] == "issueTokens" {
+		if len(parts) != 5 {
+			return types.ErrEncodingError.SetLog(cmn.Fmt("Not valid format, format should be of form 'issueTokens,account,tokens,signature,sequence'"))
+		} else {
+			userAddress := string(parts[1])
+			numTokens := parts[2]
+			signature := parts[3]
+			sequence := parts[4]
 
-		if (sequenceUint != userAccount.MaxSeenSequence + 1) {
-			return types.ErrEncodingError.SetLog(cmn.Fmt("Sequence not matching. Sequence should be %x",userAccount.MaxSeenSequence+1))
+			sequenceUint64,_ := strconv.ParseUint(sequence,10,32)
+			sequenceUint := uint32(sequenceUint64)
+
+
+			verified := VerifySignature(userAddress, numTokens, sequence, signature)
+			if !verified {
+				return types.ErrEncodingError.SetLog(cmn.Fmt("Signature not verified"))
+			}
+
+
+			userAccount := &SmartCardUser{}
+			_, userAccountBytes := accountDetails(app, userAddress)
+			ReadBinaryBytes(userAccountBytes, userAccount) //decoded user_account_bytes to user_account(of type SmartCardUser)
+			//used ReadBinaryBytes(locally implemented) and not wire.ReadBinaryBytes
+			//TODO check what to do if the account doesn't already exists
+
+
+			if (sequenceUint != userAccount.MaxSeenSequence + 1) {
+				return types.ErrEncodingError.SetLog(cmn.Fmt("Sequence not matching. Sequence should be %x",userAccount.MaxSeenSequence+1))
+			}
+
 		}
-	
-	}
-	
+	}else if parts[0] == "transact"{
+        if len(parts) != 5 {
+			return types.ErrEncodingError.SetLog(cmn.Fmt("Not valid format, format should be of form 'issueTokens,account,tokens,signature,sequence'"))
+		}else{
+            fromAddress := string(parts[1])
+            toAddress := string(parts[2])
+            numTokens := parts[3]
+            signature := parts[4]
+            sequence := parts[5]
+
+            sequenceUint64,_ := strconv.ParseUint(sequence,10,32)
+            sequenceUint := uint32(sequenceUint64)
+
+             userAccount := &SmartCardUser{}
+			_, userAccountBytes := accountDetails(app, fromAddress)
+			ReadBinaryBytes(userAccountBytes, userAccount) //decoded user_account_bytes to user_account(of type SmartCardUser)
+			//used ReadBinaryBytes(locally implemented) and not wire.ReadBinaryBytes
+
+
+			if (sequenceUint != userAccount.MaxSeenSequence + 1) {
+				return types.ErrEncodingError.SetLog(cmn.Fmt("Sequence not matching. Sequence should be %x",userAccount.MaxSeenSequence+1))
+			}
+
+
+			verified := VerifySignature(fromAddress, numTokens, sequence, signature)
+			if !verified {
+				return types.ErrEncodingError.SetLog(cmn.Fmt("Signature not verified"))
+			}
+
+        }
+    }
 	return types.OK
 }
-
 func (app *MultiIssuerPaymentApplication) DeliverTx(tx []byte) types.Result {
-	
-
 	parts := strings.Split(string(tx), ",")
-	if len(parts) != 4 {
-		return types.ErrEncodingError.SetLog(cmn.Fmt("Not valid format, format should be of form 'account,tokens,signature,sequence'"))
-	} else {
-		userAddress := string(parts[0])
-		numTokens := parts[1]
-		signature := parts[2]
-		sequence := parts[3]
-		//signature := GetSignatureIssueToken(userAddress,numTokens , sequence_str)
-		
-		sequenceUint64,_ := strconv.ParseUint(sequence,10,32)
-		sequenceUint := uint32(sequenceUint64)
-		
-		
-		userAccount := &SmartCardUser{}
-		_, userAccountBytes := accountDetails(app, userAddress)
-		ReadBinaryBytes(userAccountBytes, userAccount) //decoded user_account_bytes to user_account(of type SmartCardUser)
-		//used ReadBinaryBytes(locally implemented) and not wire.ReadBinaryBytes
+	if parts[0] == "issueTokens" {
+		if len(parts) != 5 {
+			return types.ErrEncodingError.SetLog(cmn.Fmt("Not valid format, format should be of form 'account,tokens,signature,sequence'"))
+		} else {
+			userAddress := string(parts[1])
+			numTokens := parts[2]
+			signature := parts[3]
+			sequence := parts[4]
+			//signature := GetSignatureIssueToken(userAddress,numTokens , sequence_str)
 
-		
-		if (sequenceUint != userAccount.MaxSeenSequence + 1) {
-			return types.ErrEncodingError.SetLog(cmn.Fmt("Sequence not matching. Sequence should be %x",userAccount.MaxSeenSequence+1))
-		}
-		
-		
-		verified := VerifySignature(userAddress, numTokens, sequence, signature)
-		if verified {
-			issueTokens(app, userAddress, numTokens,sequence, signature)
-		} else{
-			return types.ErrEncodingError.SetLog(cmn.Fmt("Signature not verified"))
-		}
-		
-		userAccount = &SmartCardUser{}	//couldn't use from above as userAccount is modified in issueTokens
-		_, userAccountBytes = accountDetails(app, userAddress)
-		ReadBinaryBytes(userAccountBytes, userAccount) //decoded user_account_bytes to user_account(of type SmartCardUser)
-		//used ReadBinaryBytes(locally implemented) and not wire.ReadBinaryBytes
-		
-		userAccount.MaxSeenSequence += 1
-		fmt.Println("maxseensequence", userAccount.MaxSeenSequence)
-		
-		buf := wire.BinaryBytes(userAccount) //encoded to []byte
-		app.userAccounts.Set([]byte(userAddress),buf)
+			sequenceUint64,_ := strconv.ParseUint(sequence,10,32)
+			sequenceUint := uint32(sequenceUint64)
 
-		
-		
-	}
+
+			userAccount := &SmartCardUser{}
+			_, userAccountBytes := accountDetails(app, userAddress)
+			ReadBinaryBytes(userAccountBytes, userAccount) //decoded user_account_bytes to user_account(of type SmartCardUser)
+			//used ReadBinaryBytes(locally implemented) and not wire.ReadBinaryBytes
+
+
+			if (sequenceUint != userAccount.MaxSeenSequence + 1) {
+				return types.ErrEncodingError.SetLog(cmn.Fmt("Sequence not matching. Sequence should be %x",userAccount.MaxSeenSequence+1))
+			}
+
+
+			verified := VerifySignature(userAddress, numTokens, sequence, signature)
+			if verified {
+				issueTokens(app, userAddress, numTokens,sequence, signature)
+			} else{
+				return types.ErrEncodingError.SetLog(cmn.Fmt("Signature not verified"))
+			}
+
+			userAccount = &SmartCardUser{}	//couldn't use from above as userAccount is modified in issueTokens
+			_, userAccountBytes = accountDetails(app, userAddress)
+			ReadBinaryBytes(userAccountBytes, userAccount) //decoded user_account_bytes to user_account(of type SmartCardUser)
+			//used ReadBinaryBytes(locally implemented) and not wire.ReadBinaryBytes
+
+			userAccount.MaxSeenSequence += 1
+			fmt.Println("maxseensequence", userAccount.MaxSeenSequence)
+
+			buf := wire.BinaryBytes(userAccount) //encoded to []byte
+			app.userAccounts.Set([]byte(userAddress),buf)
+
+
+
+		}
+	}else if parts[0] == "transact"{
+        if len(parts) != 5 {
+			return types.ErrEncodingError.SetLog(cmn.Fmt("Not valid format, format should be of form 'issueTokens,account,tokens,signature,sequence'"))
+		}else{
+            fromAddress := string(parts[1])
+            toAddress := string(parts[2])
+            numTokens := parts[3]
+            signature := parts[4]
+            sequence := parts[5]
+
+            sequenceUint64,_ := strconv.ParseUint(sequence,10,32)
+            sequenceUint := uint32(sequenceUint64)
+
+             userAccount := &SmartCardUser{}
+			_, userAccountBytes := accountDetails(app, fromAddress)
+			ReadBinaryBytes(userAccountBytes, userAccount) //decoded user_account_bytes to user_account(of type SmartCardUser)
+			//used ReadBinaryBytes(locally implemented) and not wire.ReadBinaryBytes
+
+
+			if (sequenceUint != userAccount.MaxSeenSequence + 1) {
+				return types.ErrEncodingError.SetLog(cmn.Fmt("Sequence not matching. Sequence should be %x",userAccount.MaxSeenSequence+1))
+			}
+
+
+			verified := VerifySignature(fromAddress, numTokens, sequence, signature)
+			if verified {
+		        transact(fromAddress,toAddress,numTokens,signature,sequence)	
+            }else{
+				return types.ErrEncodingError.SetLog(cmn.Fmt("Signature not verified"))
+            }
+        }
+    }
+
 	return types.OK
 }
 
@@ -181,75 +244,44 @@ func ReadBinaryBytes(d []byte, ptr interface{}) error {
 	return *err
 }
 
-func GetSignatureIssueToken(userAddress string, numTokens string, sequence string)string{
-	private_key := readPrivateKey()
-
-	data := map[string]string{
-		"func": "issueTokens",
-		"userAddress": userAddress,
-		"numTokens": numTokens,
-		"sequence": sequence,
-	}
-	json_data, err := json.Marshal(data)
-
-	h := sha1.New()
-	h.Write(json_data)
-	sha1_hash := hex.EncodeToString(h.Sum(nil))
-
-	r, s, err := ecdsa.Sign(rand.Reader, private_key, []byte(sha1_hash))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	signature := r.Bytes()
-	signature = append(signature, s.Bytes()...)
-	
-	signatureBigInt := new(big.Int)
-	signatureBigInt.SetBytes(signature)
-	
-	signatureString := signatureBigInt.String()
-
-	return signatureString
-}
 
 
 func VerifySignature(userAddress string, numTokens string, sequence string, signature string)bool{
 	public_key := readPublicKey()
-	
+
 	signBigInt := new(big.Int)
 	signBigInt.SetString(signature,10)
 	signBytes := signBigInt.Bytes()
-	
+
 	if len(signBytes) != 64{
 		return false
 	}else{
-	
+
 		data := map[string]string{
 			"func": "issueTokens",
 			"userAddress": userAddress,
 			"numTokens": numTokens,
 			"sequence": sequence,
 		}
-	
+
 		json_data, err := json.Marshal(data)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-	
+
 		h := sha1.New()
 		h.Write(json_data)
 		sha1_hash := hex.EncodeToString(h.Sum(nil))
-	
-	
+
+
 		r := new(big.Int)
 		r.SetBytes(signBytes[0:32])
 		s := new(big.Int)
 		s.SetBytes(signBytes[32:64])
-	
+
 		verifyStatus := ecdsa.Verify(public_key, []byte(sha1_hash), r, s)
-	
+
 		return verifyStatus
 	}
 }
@@ -264,18 +296,8 @@ func readPublicKey()*ecdsa.PublicKey{
 	return publicKey
 }
 
-func readPrivateKey()*ecdsa.PrivateKey{
-	keyEncoded, err := ioutil.ReadFile("/home/shubh/key")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	block, _ := pem.Decode(keyEncoded)
-	x509Encoded := block.Bytes
-	privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
-	return privateKey
-}
 
-//TODO add sequence
+
+//TODO add transact()
 
 
